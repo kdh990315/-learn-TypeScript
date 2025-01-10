c1059abbabab0a53fda321878eed9f823f391805
kim dong hyeon <kdh990315@naver.com>
2025-01-10 12:35:30 +0900

    리액트 + 타입스크립트 마무리

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo"
 import TodoList from "./components/TodoList"
+import { Todo } from "./todo.module";
+// import { Route } from "react-router-dom";
 
 const App = () => {
-  const todos = [
-		{
-			id: 't1',
-			text: 'Finish the course'
-		}
-	]
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const todoAddHandler = (text: string) => {
+		setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}])
+		console.log(text);
+	}
+
+	const todoDeleteHandler = (todoId: string) => { 
+		setTodos(prevTodo => {
+			return prevTodo.filter(item => item.id !== todoId);
+		})
+	}
 
   return <div className="App">
-    <TodoList items={todos}></TodoList>
+		<NewTodo onAddTodo={todoAddHandler}></NewTodo>
+    <TodoList items={todos} onDeleteTodo={todoDeleteHandler}></TodoList>
   </div>
 
 }
--- react-ts/src/components/TodoList.tsx
+++ react-ts/src/components/TodoList.tsx
@@ -1,14 +1,20 @@
+import './TodoList.css'
+
 interface TodoListProps {
   items: {
     id: string,
     text: string,
-  }[]
+  }[];
+	onDeleteTodo: (id: string) => void;
 }
 
-const TodoList = ({items}: TodoListProps) => {
+const TodoList = ({items, onDeleteTodo}: TodoListProps) => {
 	return <ul>
 		{items.map(todo => (
-			<li key={todo.id}>{todo.text}</li>
+			<li key={todo.id}>
+				<span>{todo.text}</span>
+				<button onClick={() => onDeleteTodo(todo.id)}>DELETE</button>
+			</li>
 		))}
 	</ul>
 }
