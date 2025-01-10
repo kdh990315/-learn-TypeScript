5c8cf66d836215bd71cbc83e23f02d604340ac8a
kim dong hyeon <kdh990315@naver.com>
2025-01-10 09:30:56 +0900

    react에 ts적용하기

---
+++ react-ts/src/components/TodoList.tsx
@@ -0,0 +1,16 @@
+interface TodoListProps {
+  items: {
+    id: string,
+    text: string,
+  }[]
+}
+
+const TodoList = ({items}: TodoListProps) => {
+	return <ul>
+		{items.map(todo => (
+			<li key={todo.id}>{todo.text}</li>
+		))}
+	</ul>
+}
+
+export default TodoList;
